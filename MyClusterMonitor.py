1cf3e16b409940570daa7e351fd52a049e4e73d7
Joe Czarnecki <joe.czar@outlook.com>
2020-01-23 15:36:19 +0000

    MyClusterMonitor.py working

--- MyClusterMonitor.py
+++ MyClusterMonitor.py
@@ -17,57 +17,65 @@ file = open('/home/pi/ip_list', 'r')
 data = file.readlines()
 file.close()
 
-ipAddresses = []
+ipAddresses = {}
 
-# Clean data 
-for el in data:
+# Clean data
+for idx, el in enumerate(data):
     ip = el.replace('\n', '')
-    if ip != '192.168.1.1' and ip!= '':
-        ipAddresses.append(ip)
+    if ip != '192.168.1.1' and ip != '':
+        ipAddresses[idx] = ip
 
 # Ping ips & get hostnames
-#Create a TCP/IP socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-for idx, ip in enumerate(ipAddresses):
+# Create a TCP/IP socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+for idx in ipAddresses:
+    ip = ipAddresses[idx]
     # ping each address
     rep = os.system('ping -c 1 ' + ip)
     if rep == 0:
-        print(ip, ' is up')
-        try:
-            #clear()
-            set_pixel(idx, 0, 0, 255)
-            show()
-        except:
-            print('Error')
+        ipAddresses[idx] = {"ip": ip}
+        ipAddresses[idx]["ping"] = True
+
+        # clear()
+        set_pixel(idx, 0, 0, 50)
+        show()
+
     else:
-        print(ip, ' is down')
+        ipAddresses[idx] = {"ip": ip}
+        ipAddresses[idx]["ping"] = False
+
+
 clear()
 show()
 # Check servers for ssh keys. (Adapted from - https://support.sciencelogic.com/s/article/1440)
 k = paramiko.RSAKey.from_private_key_file("/home/pi/.ssh/id_rsa")
 c = paramiko.SSHClient()
 
-for idx, ip in enumerate(ipAddresses):
-    c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    print( "connecting to ", ip)
-    try:
-        c.connect( ip, username ="pi", pkey = k )
-        try:
-            clear()
-            set_pixel(idx, 0, 128, 0)
-            show()
-        except:
-            print('Error')
-        print("connected to ", ip)
-        commands = [ "hostname", "w" ]
-        for command in commands:
-            print( "Executing {}".format( command ))
-            stdin , stdout, stderr = c.exec_command(command)
-            print( stdout.read())
-            print( "Errors")
-            print( stderr.read())
-        c.close()
-    except:
-        print("Could not connect to ", ip)
+while True:
+    for idx in ipAddresses:
+        ip = ipAddresses[idx].get("ip")
 
-# print(ipAddresses)
+        c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        try:
+            c.connect(ip, username="pi", pkey=k)
+
+            clear()
+            set_pixel(idx - 1, 0, 50, 0)
+            show()
+
+            print(idx, "connected to ", ip)
+            commands = ["python3 blinktAnswer.py all 0 0 128"]
+            for command in commands:
+
+                stdin, stdout, stderr = c.exec_command(command)
+
+            c.close()
+            ipAddresses[idx]["ssh"] = True
+        except:
+            ipAddresses[idx]["ssh"] = False
+
+            clear()
+            set_pixel(idx - 1, 128, 0, 0)
+            show()
+    time.sleep(2)
